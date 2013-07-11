a14e982e6a50d74464fcc63d8718b49f8880a2a3
amccadams <amccadams@gmail.com>
2013-07-11 07:27:08 -0500

    first commit

---
+++ js/javascript.js
@@ -0,0 +1,8 @@
+$(document).ready(function()){
+$("#filters").on("click", ".on-sale", function(){
+	$(".wine").filter(".on-sale").addClass("highlight");
+});
+$("filters").on("click",".featured", function(){
+	$(".wine").filter(".featured").addClass("highligh");
+});
+});
