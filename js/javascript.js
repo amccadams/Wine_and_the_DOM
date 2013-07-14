6a78d603a44427678e0134bb2b06f038ce2802d2
amccadams <amccadams@gmail.com>
2013-07-13 20:34:39 -0500

    added foundation and purchase page improved

--- js/javascript.js
+++ js/javascript.js
@@ -1,8 +1,8 @@
-$(document).ready(function()){
+$(document).ready(function(){
 $("#filters").on("click", ".on-sale", function(){
 	$(".wine").filter(".on-sale").addClass("highlight");
 });
 $("filters").on("click",".featured", function(){
-	$(".wine").filter(".featured").addClass("highligh");
+	$(".wine").filter(".featured").addClass("highlight");
 });
 });
